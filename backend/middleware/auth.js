ab185690dd545e97fa145bd9068eb5abcfb329c6
G-Etienne <116123659+G-Etienne@users.noreply.github.com>
2023-03-06 18:43:00 -0500

    Update auth.js

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -7,7 +7,7 @@ module.exports = (req, res, next) => {
         //Récupération du token dans le header Authorization (split --> récupère les éléments après l'espace du header)
         const token = req.headers.authorization.split(' ')[1];
         //Décodage du token
-        const decodedToken = jwt.verify(token, 'sbfsbfoumbzYT’TPGBÇV(!TAUOVJIG`’àtuh)çbrhçtu!RQEIKGÉ(GU§,J’T,H!ZÇN)TUÉ’ÉÀVÀ7Y4B1+9§4U+Z’814RV&r93té68’y88(g719+7ççk+7à(4àç+9v(1&+v4c4xr’édt’7+1+’(7g+vy+§+*1’1+x7+1rc++5v22h97(ev2');
+        const decodedToken = jwt.verify(token, '');
         //Extraction de l'ID utilisateur se trouvant dans le token
         const userId = decodedToken.userId;
         //Ajout de l'ID dans req.auth
